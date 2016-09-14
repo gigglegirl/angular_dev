2898ec33cc142d8d5105e1bfe457edd9b2e911e7
Laxmi <laxmi.singh887@gmail.com>
2016-09-14 21:09:53 +0530

    learning

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,16 +1,36 @@
 //defining all the controllers here
 
-firstApp.controller("SimpleController",function ($scope,$rootScope,simpleFactory){
+firstApp.controller("SimpleController",function ($scope,$rootScope,simpleFactory,$http){
 	$scope.customers = simpleFactory.getCustomers(); //coming data from factory
 	 $scope.counter = 0;
+	
+       /* const ref = new Firebase("https://angular-basic-app.firebaseio.com");
+        
+        ref.set("Hello London");*/
+        // download the data into a local object
+        //$scope.data = $firebaseObject(ref);
+      $scope.formData={};
+	   $scope.onSubmit=function(){
+	   	console.log($scope.formData);
+	   	console.log("I am in submit function");
 
+	   	$http.post('https://minmax-server.herokuapp.com/register/',$scope.formData)
+	   	.success(function(data){
+	   		console.log("I am succesfull")
+	   	 })
+	   	.error(function(data){
+	   		console.log("I got some erroe")
+	   	 })
+	   };
+	 
 	 //learning $watch 
-	 $scope.$watch('myText',function(newValue,oldValue){
+	 $scope.$watch('firstName',function(newValue,oldValue){
 	 $scope.counter=$scope.counter+1;
+	 $scope.newValue;
 	 })
 	 
 	 //learning rootScope
-	 $rootScope.myText="I am root scope"; //rootscope is gobal,will refelct the change in all the controllers& $scope,refelct the change only in specified controller 
+	 $rootScope.firstName="I am root scope"; //rootscope is gobal,will refelct the change in all the controllers& $scope,refelct the change only in specified controller 
 	               
     //Learning cancel route change
 	 $scope.$on("$routeChangeStart",function(event,next,current){  //1st parameter name of event which occurs when rout change is there->2nd parameter is event handler function
@@ -21,9 +41,7 @@ firstApp.controller("SimpleController",function ($scope,$rootScope,simpleFactory
 
 	 $scope.$on("emitEvent",function(a,b){
 	 	$scope.emittedMessage = b.message;
-	 })
-
- 		
+	 })	
 });
    
 
